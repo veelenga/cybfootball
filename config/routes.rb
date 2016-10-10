8a754b708873ed13a479a691378442eef93b08ff
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-10-10 15:29:32 +0300

    Generate bootstrap layout & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :tournaments
   resources :teams
   resources :players
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'tournaments#index'
 end
