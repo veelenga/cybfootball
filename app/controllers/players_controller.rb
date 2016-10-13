599773bff6931ba1fdb2237aa912e1f73af134d7
Vitalii Elenhaupt <veelenga@users.noreply.github.com>
2016-10-13 09:15:59 +0300

    Instant player search (#1)
    * Add instant search on players#index
    
    * Make instant search more general
    
    * Add timeout to ajax call and delay between data lookups

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -61,6 +61,10 @@ class PlayersController < ApplicationController
     end
   end
 
+  def search
+    @players = Player.search player_params[:fio]
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_player
