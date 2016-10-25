4fedd989d293e80a22277fa270e76c4483b811ac
Vitalii Elenhaupt <veelenga@users.noreply.github.com>
2016-10-25 16:05:31 +0300

    Internationalization support (#12)
    * Basic internationalization setup
    
    * Save locale across requests
    
    * Active record translations
    
    Closes #5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   protected
 
@@ -7,6 +8,14 @@ class ApplicationController < ActionController::Base
     per = JSON.parse(cookies[:per]) rescue {}
     params[:per] ||= per[page_name] if per.present?
     cookies[:per] = JSON.generate({ page_name => params[:per] }.reverse_merge! per) if params[:per].present?
+  end
+
+  def set_locale
+    I18n.locale = I18n.available_locales.include?(params[:locale].try :to_sym) ? params[:locale] : I18n.default_locale
+  end
+
+  def default_url_options
+    I18n.locale != I18n.default_locale ? { locale: I18n.locale } : {}
   end
 
   private
