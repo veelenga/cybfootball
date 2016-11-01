99cf91af07836bc5a7554cab58f1f2868b9f79d5
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-11-01 15:50:41 +0200

    Avatar chooser for player & team

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,5 @@
 class Player < ApplicationRecord
-  AVATAR_GEOMETRY = '300x300#'
-
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  include ModelExtensions::Avatar
 
   has_and_belongs_to_many :teams, -> { distinct }
   has_many :goals
@@ -9,28 +7,14 @@ class Player < ApplicationRecord
   validates_presence_of :fio
 
   after_initialize :set_default_attributes, if: :new_record?
-  after_update :reprocess_avatar, :if => :crop?
 
   scope :fio_like, ->(fio) { where('players.fio ~* ?', ".*#{fio}.*") }
 
-  has_attached_file :avatar, styles: { thumb: AVATAR_GEOMETRY }, default_url: "/images/default_avatar.png", processors: [:crop]
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
-  def crop?
-    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
-  end
-
-  def geometry
-    @geometry ||= Paperclip::Geometry.parse AVATAR_GEOMETRY
-  end
+  has_avatar styles: {thumb: '300x300#'}, default_url: "/images/default_avatar.png"
 
   private
 
   def set_default_attributes
     self.graduation_year ||= Date.today.year
-  end
-
-  def reprocess_avatar
-    avatar.reprocess!
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
+  include ModelExtensions::Avatar
+
   has_and_belongs_to_many :players, -> { distinct }
   has_many :inhouse_matches, class_name: 'Match', inverse_of: :home_team
   has_many :matches_as_a_guest, class_name: 'Match', inverse_of: :guest_team
@@ -8,8 +10,7 @@ class Team < ApplicationRecord
 
   scope :by_name, ->(name) { where('teams.name ~* ?', ".*#{name}.*")}
 
-  has_attached_file :avatar, styles: { thumb: '300x200>' }, default_url: '/images/default_team_avatar.png'
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_avatar styles: {thumb: '300x200>'}, default_url: "/images/default_team_avatar.png"
 
   def update_players_list(player, action)
     case action.try(:to_sym)
