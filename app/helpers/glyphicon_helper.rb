c124f7a5459000ff66e3ea77e284ee9b5c2fdc27
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-01-25 18:50:25 +0200

    More control btns

--- app/helpers/glyphicon_helper.rb
+++ app/helpers/glyphicon_helper.rb
@@ -1,45 +1,21 @@
 module GlyphiconHelper
-  def tournament_header_glyph
-    content_tag(:i, nil, class: 'fa fa-trophy faa-tada animated-hover')
+
+  def glyph(clazz, animation)
+    content_tag(:i, nil, class: "#{clazz} faa-#{animation} animated-hover")
   end
 
-  def teams_header_glyph
-    content_tag(:i, nil, class: 'fa fa-users faa-tada animated-hover')
-  end
+  def tournament_header_glyph; glyph('fa fa-trophy', :tada) end
+  def teams_header_glyph;      glyph('fa fa-users', :tada) end
+  def players_header_glyph;    glyph('fa fa-user', :tada) end
+  def groups_header_glyph;     glyph('fa fa-table', :tada) end
 
-  def players_header_glyph
-    content_tag(:i, nil, class: 'fa fa-user faa-tada animated-hover')
-  end
+  def add_glyph;    glyph('fa fa-plus', :pulse) end
+  def edit_glyph;   glyph('glyphicon glyphicon-pencil', :pulse) end
+  def delete_glyph; glyph('glyphicon glyphicon-trash', :pulse) end
+  def remove_glyph; glyph('glyphicon glyphicon-remove', :pulse) end
+  def back_glyph;   glyph('glyphicon glyphicon-chevron-left', :pulse) end
 
-  def groups_header_glyph
-    content_tag(:i, nil, class: 'fa fa-table faa-tada animated-hover')
-  end
-
-  def add_glyph
-    content_tag(:i, nil, class: 'fa fa-plus faa-pulse animated-hover')
-  end
-
-  def edit_glyph
-    content_tag(:i, nil, class: 'glyphicon glyphicon-pencil faa-pulse animated-hover')
-  end
-
-  def delete_glyph
-    content_tag(:i, nil, class: 'glyphicon glyphicon-trash faa-pulse animated-hover')
-  end
-
-  def remove_glyph
-    content_tag(:i, nil, class: 'glyphicon glyphicon-remove faa-pulse animated-hover')
-  end
-
-  def back_glyph
-    content_tag(:i, nil, class: 'glyphicon glyphicon-chevron-left faa-pulse animated-hover')
-  end
-
-  def add_group_glyph
-    content_tag(:i, nil, class: 'fa fa-plus faa-pulse animated-hover')
-  end
-
-  def settings_glyph
-    content_tag(:i, nil, class: 'fa fa-cog faa-spin animated-hover')
-  end
+  def add_group_glyph;  glyph('fa fa-table', :pulse) end
+  def add_player_glyph; glyph('fa fa-user', :pulse) end
+  def settings_glyph;   glyph('fa fa-cog', :spin) end
 end
